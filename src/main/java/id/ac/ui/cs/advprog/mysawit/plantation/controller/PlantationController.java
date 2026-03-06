2c44c9157a07cdb9079c5e2c540da45cec8e96f1
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2026-03-06 14:34:56 +0700

    feat: add Controller for plantation

--- src/main/java/id/ac/ui/cs/advprog/mysawit/plantation/controller/PlantationController.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/plantation/controller/PlantationController.java
@@ -2,10 +2,13 @@ package id.ac.ui.cs.advprog.mysawit.plantation.controller;
 
 import id.ac.ui.cs.advprog.mysawit.plantation.dto.CreatePlantationRequest;
 import id.ac.ui.cs.advprog.mysawit.plantation.dto.PlantationResponse;
+import id.ac.ui.cs.advprog.mysawit.plantation.dto.UpdatePlantationRequest;
 import id.ac.ui.cs.advprog.mysawit.plantation.service.PlantationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/plantations")
@@ -17,5 +20,13 @@ public class PlantationController {
     @PostMapping
     public PlantationResponse createPlantation(@Valid @RequestBody CreatePlantationRequest request) {
         return plantationService.createPlantation(request);
+    }
+
+    @PutMapping("/{id}")
+    public PlantationResponse updatePlantation(
+            @PathVariable UUID id,
+            @Valid @RequestBody UpdatePlantationRequest request
+    ) {
+        return plantationService.updatePlantation(id, request);
     }
 }
