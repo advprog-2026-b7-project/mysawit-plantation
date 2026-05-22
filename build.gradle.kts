beea0d2b8d422e4271fa78f0bab31ec1aa663610
amberleyvs <amberley.vidya41@ui.ac.id>
2026-05-22 20:30:33 +0700

    feat: add Spring Boot Actuator for profiling

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.h2database:h2")
